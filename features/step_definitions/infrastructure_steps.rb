fefca6ba147a4b120cf7838ebacc457928d181ba
Rob Eastwood <re.web.accounts@protonmail.com>
2022-09-15 18:58:37 +1000

    Refactor and document

--- features/step_definitions/infrastructure_steps.rb
+++ features/step_definitions/infrastructure_steps.rb
@@ -1,15 +1,7 @@
 
-Given('a test file named {string} exists') do |testfile|
-  @testfile = testfile
-  expect(@testfile).to be_in_testcase_directory
-end
-
-When('I run the validator command on this testfile against profile {string}') do |profile_id|
-
-  cmd = TTY::Command.new(printer: :null)
-
-  # determine if the feature is being run on Windows or on Linux (GitHub action)
-  # on Windows 
+# determine if the feature is being run on Windows or on Linux (GitHub action)
+def get_validator_path
+  # on windows
   if Gem::Platform.local.os == "mingw32"
     # on my personal laptop
     if Socket.gethostname == "LAPTOP-KL85GOQC"
@@ -21,8 +13,22 @@ When('I run the validator command on this testfile against profile {string}') do
   else
     path_to_validator = "validator.jar"
   end
+  return path_to_validator
+end
 
-   profile_url = "#{PROFILE_BASE}#{profile_id}"
+
+Given('a test file named {string} exists') do |testfile|
+  @testfile = testfile
+  expect(@testfile).to be_in_testcase_directory
+end
+
+When('I run the validator command on this testfile against profile {string}') do |profile_id|
+
+  cmd = TTY::Command.new(printer: :null)
+
+  path_to_validator = get_validator_path()
+
+  profile_url = "#{PROFILE_BASE}#{profile_id}"
 
   # term_server = "https://r4.ontoserver.csiro.au/fhir"
   # term_server = "n/a"
@@ -32,18 +38,23 @@ When('I run the validator command on this testfile against profile {string}') do
 
   begin
 
+    # first check if validator is present
+    # and if not, manage exception
     if not(File.exist?(path_to_validator))
       raise ValidatorNotPresentError
+    # otherwise run the validator
     else
       Kernel.puts "      Running validation command..."
       @output, @err = cmd.run(validator_command)
     end
   
+  # rescue errors caused by validation errors
   rescue TTY::Command::ExitError => e
     # Kernel.puts e.to_s
     @output = e.to_s
     true
 
+  # rescue the exception when the validator is not found
   rescue ValidatorNotPresentError => e
     # Kernel.puts e.to_s
     expect(path_to_validator).to be_accessible
